ccba9834f1d21b8e7d8edcbf3a75cd5cabb3a5fb
Anthony <alci@gmail.com>
2025-05-15 12:15:52 -0500

    Semana8 v2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
 import { CategoryComponent } from './pages/category/category.component';
 import { PublisherComponent } from './pages/publisher/publisher.component';
+import { PublisherEditComponent } from './pages/publisher/publisher-edit/publisher-edit.component';
 
 export const routes: Routes = [
     {path: 'pages/category',component: CategoryComponent},
-    {path: 'pages/publisher', component: PublisherComponent}
+    {path: 'pages/publisher', component: PublisherComponent,
+        children: [
+                //pages/publisher/2
+            {path: 'new', component: PublisherEditComponent},
+                //pages/publisher/new
+            {path: 'edit/:id', component: PublisherEditComponent},
+        ]
+    }
+
+
 ];
--- src/app/pages/publisher/publisher.component.ts
+++ src/app/pages/publisher/publisher.component.ts
@@ -1,14 +1,21 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild, viewChild } from '@angular/core';
 import { PublisherService } from '../../services/publisher.service';
 import { Publisher } from '../../model/publisher';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import { MatFormField } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-publisher',
-  imports: [MatTableModule, MatFormField, MatInput],
+  imports: [MatTableModule, MatFormField, MatInput,MatButton,MatIconModule, MatPaginatorModule,
+    MatSortModule,RouterLink,RouterOutlet
+  ],
   templateUrl: './publisher.component.html',
   styleUrl: './publisher.component.css'
 })
@@ -17,6 +24,18 @@ export class PublisherComponent {
   displayedColumns: string[] = ['idPublisher', 'name', 'address'];
   dataSource: MatTableDataSource<Publisher>;
   //constructor(private publisherService: PublisherService){}
+ 
+  //arreglo personalizado
+  columDefinitions = [
+    {def: 'idPublisher', label: 'idPublisher', hide: true},
+    {def: 'name', label: 'name', hide: false},
+    {def: 'address', label: 'address', hide: false},
+    {def: 'actions', label: 'actions', hide: false},
+  ];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
   publisherService = inject(PublisherService);
 
   ngOnInit():void{
@@ -24,11 +43,19 @@ export class PublisherComponent {
     //this.publisherService.findAll().subscribe(data => this.publishers = data);
     this.publisherService.findAll().subscribe(data => {
       this.dataSource= new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     }); 
   }
 
+  //metodo para habilitar la busqueda
   applyFilter(e : any){
     this.dataSource.filter = e.target.value.trim();
+  }
+
+  //metodo mostrar columnas que no tienen el atributo hide=true
+  getDisplayColums(){
+    return this.columDefinitions.filter(cd => !cd.hide).map(cd => cd.def);
   }
 
 }
